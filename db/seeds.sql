366b83c7992c073ebbffadf512c11204c6d42d85
snw380 <ty.morgan818@gmail.com>
2020-09-23 21:09:01 -0400

    established the db and connection

---
+++ db/seeds.sql
@@ -0,0 +1,7 @@
+###Seeds
+
+USE burgers_db;
+
+INSERT INTO burgers (burger_name) VALUES ('cheeseburger');
+INSERT INTO burgers (burger_name) VALUES ('bacon cheeseburger');
+INSERT INTO burgers (burger_name) VALUES ('veggie burger');

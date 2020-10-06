85b55ab6931a934e8e23d2c800bf67d208cf3d4c
snw380 <ty.morgan818@gmail.com>
2020-10-06 14:37:38 -0400

    finished front end and added functionality

---
+++ public/js/burgers.js
@@ -0,0 +1,36 @@
+$(document).ready(function () {
+    $(".create-form").on("submit", function (event) {
+
+        event.preventDefault();
+        var newBurger = {
+            burger_name: $("#burger").val().trim(),
+            devoured: 0
+        };
+        $.ajax("/api/burgers", {
+            type: "POST",
+            data: newBurger
+        }).then(
+            function () {
+                console.log("Added a new burger!");
+                location.reload();
+                console.log("newBurger", newBurger)
+            }
+        );
+    });
+
+    $(".devour").on("click", function () {
+        var id = $(this).attr("data-id");
+        var devBurger = {
+            devoured: 1
+        };
+        $.ajax("/api/burgers/" + id, {
+            type: "PUT",
+            data: devBurger
+        }).then(
+            function () {
+                console.log("You ate the burger!");
+                location.reload();
+            }
+        );
+    })
+});

cf4dd38933c46a07152030a79f2a0f895568c03d
snw380 <ty.morgan818@gmail.com>
2020-10-08 21:11:37 -0400

    updated connection

--- config/connection.js
+++ config/connection.js
@@ -12,16 +12,17 @@ if (process.env.JAWSDB_URL) {
     password: "Unlyriced1!",
     database: "burgers_db"
   });
-}
-
-// Make connection.
-connection.connect(function (err) {
+  // Make connection.
+  connection.connect(function (err) {
   if (err) {
     console.error("error connecting: " + err.stack);
     return;
   }
   console.log("connected as id " + connection.threadId);
 });
+
+}
+
 
 // Export connection for our ORM to use.
 module.exports = connection;
